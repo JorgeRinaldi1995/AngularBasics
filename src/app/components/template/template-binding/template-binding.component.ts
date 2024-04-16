1f5aee71ff8a5f4967423debe8881180beb7f5c5
JorgeRinaldi1995 <jorge.rinaldi.1995@gmail.com>
2024-04-16 15:23:17 -0300

    data binding basics

--- src/app/components/template/template-binding/template-binding.component.ts
+++ src/app/components/template/template-binding/template-binding.component.ts
@@ -1,11 +1,12 @@
-import { CommonModule } from '@angular/common';
+import { CommonModule, NgClass } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 
 @Component({
   selector: 'app-template-binding',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule, NgClass],
   templateUrl: './template-binding.component.html',
   styleUrl: './template-binding.component.scss'
 })
@@ -16,9 +17,22 @@ export class TemplateBindingComponent {
   public isDisabled = this.lightersHeStolen === 0 ? true : false;
   public imgSrc = 'https://scontent.fsjp3-1.fna.fbcdn.net/v/t39.30808-6/308988886_440149911546728_8231162373279455997_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=5f2048&_nc_ohc=t1H_tnIvJjUAb7jNdVS&_nc_ht=scontent.fsjp3-1.fna&oh=00_AfCUOSd73ZWA5V4TjFfpLV0E5UtNHZD1XaYwed9At0kKNQ&oe=661F50D7'
 
-  
-
   public howManyJointsTheySmoke(joints: number, smokesPerDay: number) {
-    return joints * smokesPerDay;
+    var result = joints * smokesPerDay
+    return result;
+  }
+
+  public sum(){
+    return this.lightersHeStolen++
+  }
+  public sub(){
+    return this.lightersHeStolen--
+  }
+  public onKeyDown(event: Event){
+    return console.log(event)
+  }
+
+  public onMouseMove(event: Event){
+    return console.log(event)
   }
 }
